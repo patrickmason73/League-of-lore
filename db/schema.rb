6fcc4503e3fee52a1b84be7876814b7131ee6680
Patrick Mason <patrickmason73@gmail.com>
2023-11-26 16:48:39 -0600

    schema

--- db/schema.rb
+++ db/schema.rb
@@ -63,38 +63,8 @@ ActiveRecord::Schema.define(version: 2023_11_26_221538) do
     t.index ["champion_region_id"], name: "index_champions_on_champion_region_id"
   end
 
-  create_table "comments", force: :cascade do |t|
-    t.bigint "user_id", null: false
-    t.bigint "post_id", null: false
-    t.text "content"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-    t.index ["post_id"], name: "index_comments_on_post_id"
-    t.index ["user_id"], name: "index_comments_on_user_id"
-  end
-
-  create_table "posts", force: :cascade do |t|
-    t.string "title"
-    t.text "content"
-    t.string "img_url"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-  end
-
-  create_table "users", force: :cascade do |t|
-    t.string "username"
-    t.string "password_digest"
-    t.string "display_name"
-    t.text "bio"
-    t.string "img_url"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-  end
-
   add_foreign_key "abilities", "champions"
   add_foreign_key "champion_comments", "capstone_users"
   add_foreign_key "champion_comments", "champions"
   add_foreign_key "champions", "champion_regions"
-  add_foreign_key "comments", "posts"
-  add_foreign_key "comments", "users"
 end
