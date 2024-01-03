8ba2e9f9809e1d9c0dbedec4ba330e9dce7acf20
Patrick Mason <patrickmason73@gmail.com>
2024-01-03 14:34:56 -0600

    user profile style changes

--- client/src/components/UserProfile.js
+++ client/src/components/UserProfile.js
@@ -58,7 +58,7 @@ function UserProfile({ champions, userPosts, updateUser }) {
     const displayPosts = currentUserPosts.map((post) => {
         if (post !== null) {
         return (
-        <div key={post.id} style={{borderStyle: "groove", margin: "14px", borderWidth: "min-content", borderColor: "black", marginTop: "0", width: "fit-content"}}>
+        <div key={post.id} style={{borderStyle: "groove", marginRight: "auto", marginLeft: "auto", marginBottom: "15px", borderWidth: "min-content", borderColor: "black", width: "fit-content", textAlign: "center"}}>
 
             <span style={{fontWeight: "600", paddingLeft: "5px", marginBottom: "5px", marginTop: "0px", marginLeft: "2px", textAlign: "center", width: "70%", fontSize: "150%"}}>
              <div style={{marginTop: "0px"}}><img src={post.img_url} alt="post-img" style={{width: "50%", textAlign: "center", maxWidth: "300px", borderStyle: "groove", borderColor: "black"}}/></div>
@@ -74,7 +74,7 @@ function UserProfile({ champions, userPosts, updateUser }) {
         <div style={{ backgroundImage: "linear-gradient(to right, #fc5c7d, #6a82fb)", width: "100%", paddingTop: "10px", height: "100%"}}>
             <span style={{display: "flex", alignItems: "baseline", columnFill: "balance"}}>
             <span style={{marginRight: "auto", marginLeft: "80px", width: "600px"}}>
-                 <div style={{fontSize: "150%", textAlign: "left", fontWeight: "500", borderStyle: "solid", padding: "3px", marginTop: "20px", paddingBottom: "0px", maxWidth: "380px"}}><u>Display Name:</u><span style={{fontWeight: "800"}}> {currentUser.display_name} <br /> <button style={{backgroundColor: "black", color: "white", cursor: "pointer", fontSize: "55%", fontWeight: "600", borderRadius: "4px", marginLeft: "30px", marginBottom: "5px"}} onClick={() => setEditingName(current => !current)}>{editingName ? "Cancel" : "Edit Display Name"}</button></span>
+                 <div style={{fontSize: "150%", textAlign: "left", fontWeight: "500", borderStyle: "solid", borderRadius: "5px", padding: "3px", marginTop: "20px", paddingBottom: "0px", maxWidth: "380px", marginLeft: "auto", marginRight: "auto"}}><u>Display Name:</u><span style={{fontWeight: "800"}}> {currentUser.display_name} <br /> <button style={{backgroundColor: "black", color: "white", cursor: "pointer", fontSize: "55%", fontWeight: "600", borderRadius: "4px", marginLeft: "30px", marginBottom: "5px"}} onClick={() => setEditingName(current => !current)}>{editingName ? "Cancel" : "Edit Display Name"}</button></span>
                     {editingName ? 
                         <form onSubmit={handleUpdateUser} >
                             <label>
@@ -94,13 +94,13 @@ function UserProfile({ champions, userPosts, updateUser }) {
                         </form>
                     :null}
                  </div>
-                 <div style={{textAlign: "center", marginTop: "20px", marginBottom: "20px"}}><img src={currentUser.profile_pic} alt="profilePic" style={{ textAlign: "center", marginLeft: "auto", marginRight: "auto", maxWidth: "300px", borderStyle: "groove", borderColor: "black"}}/></div>
-                 <div style={{ marginTop: "10px"}}><p style={{marginBottom: "0px", fontWeight: "700", fontSize: "150%", textDecorationLine: "underline", marginLeft: "14px", marginTop: "0"}}>Your Posts:</p><br/>{displayPosts}</div>
+                 <div style={{ marginTop: "20px", marginBottom: "20px", textAlign: "center"}}><img src={currentUser.profile_pic} alt="profilePic" style={{ textAlign: "center", marginLeft: "auto", marginRight: "auto", maxWidth: "300px", borderStyle: "groove", borderColor: "black"}}/></div>
+                 <div style={{ marginTop: "10px", borderStyle: "groove", borderColor: "black", borderRadius: "5px", borderWidth: "5px", textAlign: "center"}}><p style={{marginBottom: "0px", fontWeight: "800", fontSize: "175%", textDecorationLine: "underline", marginLeft: "14px", marginTop: "0"}}>Your Posts:</p><br/>{displayPosts}</div>
             </span>
                 <img src={image} alt="divider" style={{ width: "auto", alignSelf: "center"}}/>
             <span style={{marginLeft: "auto", marginRight: "80px", width: "600px"}}>
-                <span style={{verticalAlign: "top", fontWeight: "500", marginRight: "100px", fontSize: "125%", marginBottom: "300px", paddingLeft: "14px"}}> <b style={{textDecorationLine: "underline", textAlign: "left"}}>Bio:</b><br/> <div style={{paddingLeft: "14px"}}>{currentUser.bio}</div> <br style={{marginBottom: "15px"}}/> </span>
-                <div style={{marginRight: "80px", marginLeft: "auto", textAlign: "center"}}><b style={{textDecoration: "underline", paddingTop: "40px", marginTop: "100px" }}>Champions You Commented On:</b> <br/> {displayChamps} </div>
+                <span style={{verticalAlign: "top", fontWeight: "500", marginRight: "100px", fontSize: "125%", marginBottom: "300px", paddingLeft: "14px"}}> <b style={{textDecorationLine: "underline", textAlign: "left", fontWeight: "800"}}>Bio:</b><br/> <div style={{paddingLeft: "14px"}}>{currentUser.bio}</div> <br style={{marginBottom: "15px"}}/> </span>
+                <div style={{marginRight: "80px", marginLeft: "auto", textAlign: "center", borderStyle: "groove", borderColor: "black", borderWidth: "5px", marginTop: "40px"}}><b style={{textDecoration: "underline", paddingTop: "40px", marginTop: "100px", fontWeight: "800", fontSize: "120%" }}>Champions You Commented On:</b> <br/> {displayChamps} </div>
             </span>
             </span>
             <p style={{color: "GrayText", textAlign: "center", WebkitTextStroke: "0.3px black", marginBottom: "0", paddingBottom: "20px"}}> <i>“Knowing yourself is the beginning of all wisdom.” -Aristotle</i></p>
