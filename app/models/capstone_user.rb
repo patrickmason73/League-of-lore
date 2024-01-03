108df8ce661a8114377f00b8b279218f56d55532
Patrick Mason <patrickmason73@gmail.com>
2024-01-02 18:15:49 -0600

    validate fix

--- app/models/capstone_user.rb
+++ app/models/capstone_user.rb
@@ -9,5 +9,4 @@ class CapstoneUser < ApplicationRecord
     validates :username, presence: true, uniqueness: true, length: { maximum: 20, minimum: 8 }
     validates :bio, presence: true, length: { maximum: 1000 }
     validates :display_name, presence: true, uniqueness: true, length: { maximum: 12, minimum: 5 }
-    validates :password, uniqueness: true, length: { minimum: 8, maximum: 20 }
 end
