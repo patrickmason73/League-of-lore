fa19887d148aaf52d52e394da151b3b79af72868
Patrick Mason <patrickmason73@gmail.com>
2024-01-05 16:55:59 -0600

    error in users serializer fix

--- app/serializers/capstone_users_serializer.rb
+++ app/serializers/capstone_users_serializer.rb
@@ -1,3 +1,3 @@
 class CapstoneUsersSerializer < ActiveModel::Serializer
-  attributes :id, :username, :password, :password_confirmation, :display_name, :profile_pic, :bio, :email, :champion_comments, :champions, :user_posts, :post_comments, :capstone_user, 
+  attributes :id, :username, :password, :password_confirmation, :display_name, :profile_pic, :bio, :email, :champion_comments, :champions, :user_posts, :post_comments, :capstone_user 
 end
